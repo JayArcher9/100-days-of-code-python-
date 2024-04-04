b6ba85686859a5a3cf18e0fb981ab351cfb74e40
JayArcher9 <143174572+JayArcher9@users.noreply.github.com>
2024-04-04 14:34:48 +0530

    Add files via upload

---
+++ Daily Projects/21. Pong Game/ball.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+import pygame
+
+
+pygame.init()
+
+# Initialize the mixer module
+pygame.mixer.init()
+
+# Now you can use sound-related functions like loading and playing sounds
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)     
+
+    def bounce_y(self):
+        self.y_move *= -1
+        
+    
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+    def l_point(self):
+        self.goto(0, 0)
+        self.bounce_x()
+       
+    def r_point(self):
+        self.goto(0, 0)
+        self.bounce_x()
+      
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+      
+       
